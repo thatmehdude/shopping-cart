b37f87477ac8411df6fc4a16b22a35786f458703
Chisom <anaelechisom16@gmail.com>
2025-03-14 15:50:37 +0100

    delete clutter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,3 @@
-// const Products = () => {
-//     return (
-//         <div className="p-4">
-//             <h1 className="text-2xl font-bold">Our available products</h1>
-//             <p className="mt-2">Browse our products and add them to your cart</p>
-//         </div>
-//     );
-// }
-
-// export default Products;
-
-// src/pages/Products.jsx
 import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { getProducts, getCategories } from '../services/api';
